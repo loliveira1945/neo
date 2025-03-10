a65d8817131b491563a4105910aab0d825fc96eb
Lucas Oliveira <lucasalves1945@icloud.com>
2025-03-10 20:01:02 -0300

    Ajuste grid

--- src/pages/ArticlePage.vue
+++ src/pages/ArticlePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center pt-[60px]">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center pt-[60px]">
     <ArticleCard
       v-for="article in articles"
       :key="article.id"
